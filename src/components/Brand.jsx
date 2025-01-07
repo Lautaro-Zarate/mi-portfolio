0991b749b491a3517ff0d30c47b47b4b47109ccc
Lautaro Zarate <zlautaro43@gmail.com>
2025-01-06 21:48:19 -0300

    Corrección en README.md

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,7 +1,7 @@
 const Brand = () => {
     return(
         <div>
-            <img src="/public/brand.png" alt="Logo" className="brand"/>
+            <img src="./public/brand.png" alt="Logo" className="brand"/>
         </div>
     )   
 }
